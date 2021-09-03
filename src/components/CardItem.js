bb632d418fed7a92bcbba9e7a02aa0fbd531d2c8
KarinaPiestun <kpiestun@icloud.com>
2021-09-03 16:58:28 -0300

    pages

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -6,12 +6,15 @@ function CardItem(props) {
     return (
         <>
 
-        <li className='cards_item'>
+        <li className='cards__item'>
             <Link className='cards__item__link' to={props.path}>
             
             <figure className='cards__item__pic-wrap' data-category={props.label} > 
-            <img src={props.src}  alt='Travel Image'
-            className='cards__item__img'/>
+            <img
+            className='cards__item__img'
+            alt='Travel Image'
+            src={props.src}
+            />
             </figure>
 
             <div className='cards__item__info'>
